dc12fe2e45189f661048b4e571f9f659160edb10
Alexander Power <powera@gmail.com>
2024-11-20 14:52:04 -0600

    run benchmark for spell_check
    qwen2.5:7b gets a 93/100 on this.

--- lib/run_benchmark.py
+++ lib/run_benchmark.py
@@ -11,21 +11,38 @@ def run_0015_spell_check(model):
   DIR = "benchmarks/0015_spell_check"
 
   sentence_list = []
-
   for filename in os.listdir(DIR):
     if filename.endswith(".json"):
       word = filename[:-5]
       with open(os.path.join(DIR, filename)) as f:
         sentences_raw = json.load(f)
         for s in sentences_raw:
-          sentence_list.append({"sentence": s, "word": word})
+          sentence_list.append(s)
 
-  results = []
+      results = []
+
+  total_questions = 0
+  correct_answers = 0
   for x in sentence_list:
     prompt = f"""
 What is the incorrectly-spelled word in this sentence: {x["sentence"]}
 
-When responding, give the incorrect spelling, followed by a hyphen, followed by the correct spelling."""
+When responding, give the incorrect spelling, followed by a space, hyphen, space, and the correct spelling.
+
+Two example response:
+  bigg - big
+  chainge - change
+"""
 
     response, _ = ollama_client.generate_chat(prompt, model)
-    print(response)
+    total_questions += 1
+    response_parts = response.split()
+    if len(response_parts) == 3:
+      response_wrong = response.split()[0]
+      response_right = response.split()[2]
+      if x["incorrect"] == response_wrong and x["correct"] == response_right:
+        correct_answers += 1
+        continue
+    print(f"WRONG: Sentence << {x['sentence']} >>, Response << {response} >>")
+
+  print(f"RESULTS: {correct_answers} correct of {total_questions} questions.")
