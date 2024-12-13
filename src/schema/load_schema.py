edf25f14e2d6d5f8187426762694d6479a5e7e1d
Alexander Power <powera@gmail.com>
2024-12-12 18:34:36 -0600

    rm "metric" from DB schema
    also cleanup

--- src/schema/load_schema.py
+++ src/schema/load_schema.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python3
 
+import os.path
+
 import sqlite3
 
-# Define the database file
-db_file = "benchmarks.db"
+import constants
 
-# Define the schema file
-schema_file = "benchmarks.sql"
+def create_tables():
+    # Define the database file
+    db_file = os.path.join(constants.SCHEMA_DIR, "benchmarks.db")
 
-# Connect to the database
-with sqlite3.connect(db_file) as conn:
-    # Read the schema file
-    with open(schema_file, "r") as f:
-        schema = f.read()
-    # Execute the schema
-    conn.executescript(schema)
-    print("Database and schema created successfully.")
+    # Define the schema file
+    schema_file = os.path.join(constants.SCHEMA_DIR, "benchmarks.sql")
+
+    # Connect to the database
+    with sqlite3.connect(db_file) as conn:
+        # Read the schema file
+        with open(schema_file, "r") as f:
+            schema = f.read()
+        # Execute the schema
+        conn.executescript(schema)
+        print("Database and schema created successfully.")
+
+if __name__ == "__main__":
+  create_tables()
