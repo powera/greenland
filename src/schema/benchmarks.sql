dc609cd129f5f566ea6dbc55149c377f863b319c
Alexander Power <powera@gmail.com>
2024-12-12 18:34:36 -0600

    rm "metric" from DB schema
    also cleanup

--- src/schema/benchmarks.sql
+++ src/schema/benchmarks.sql
@@ -9,12 +9,10 @@ CREATE TABLE model (
 );
 
 CREATE TABLE benchmark (
-  codename TEXT NOT NULL,
-  metric TEXT NOT NULL,
+  codename TEXT NOT NULL PRIMARY KEY,
   displayname TEXT NOT NULL,
   description TEXT,
-  license_name TEXT,
-  PRIMARY KEY (codename, metric)
+  license_name TEXT
 );
 
 CREATE TABLE question (
@@ -29,23 +27,21 @@ CREATE TABLE run (
   run_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   model_name TEXT,
   benchmark_name TEXT,
-  benchmark_metric TEXT,
   normed_score INTEGER,
   FOREIGN KEY (model_name) REFERENCES model (codename),
-  FOREIGN KEY (benchmark_name, benchmark_metric) REFERENCES benchmark (codename, metric)
+  FOREIGN KEY (benchmark_name) REFERENCES benchmark (codename)
 );
 
 CREATE TABLE run_detail (
   run_id INTEGER NOT NULL,
   question_id TEXT NOT NULL,
   benchmark_name TEXT,
-  benchmark_metric TEXT,
   score INTEGER,
   eval_msec INTEGER,
   debug_json TEXT,
   PRIMARY KEY (run_id, question_id),
   FOREIGN KEY (run_id) REFERENCES run (run_id),
   FOREIGN KEY (question_id) REFERENCES question (question_id)
-  FOREIGN KEY (benchmark_name, benchmark_metric) REFERENCES benchmark (codename, metric)
+  FOREIGN KEY (benchmark_name) REFERENCES benchmark (codename)
 );
 
