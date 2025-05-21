06a6ebdacd26e1a4c27792c1c572517d0fa7e1a5
Alexander Power <powera@gmail.com>
2025-05-21 10:49:58 -0500

    move schema definitions out of linguistic_db.py

---
+++ src/wordfreq/models/__init__.py
@@ -0,0 +1,4 @@
+"""Models package for wordfreq."""
+
+from wordfreq.models.schema import Base, Word, Definition, Example, QueryLog, Corpus, WordFrequency
+from wordfreq.models.enums import NounSubtype, VerbSubtype, AdjectiveSubtype, AdverbSubtype
