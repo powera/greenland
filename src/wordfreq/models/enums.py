2a51b2df362f3612158524ddf5e497aacb03e501
Alexander Power <powera@gmail.com>
2025-05-21 13:58:12 -0500

    update the "noun taxonomy" to have 30 categories + "other"

--- src/wordfreq/models/enums.py
+++ src/wordfreq/models/enums.py
@@ -6,24 +6,53 @@ import enum
 
 class NounSubtype(enum.Enum):
     """Subtypes for nouns."""
+    # People and Living Things
     HUMAN = "human"
     ANIMAL = "animal"
+    BODY_PART = "body_part"
+    DISEASE_CONDITION = "disease_condition"
     PLANT = "plant"
+    
+    # Food and Consumables
+    FOOD_DRINK = "food_drink"
+    
+    # Physical Objects and Structures
     BUILDING_STRUCTURE = "building_structure"
     SMALL_MOVABLE_OBJECT = "small_movable_object"
+    CLOTHING_ACCESSORY = "clothing_accessory"
+    ARTWORK_ARTIFACT = "artwork_artifact"
     NATURAL_FEATURE = "natural_feature"
     TOOL_MACHINE = "tool_machine"
+    PATH_INFRASTRUCTURE = "path_infrastructure"
+    
+    # Materials and Substances
     MATERIAL_SUBSTANCE = "material_substance"
+    CHEMICAL_COMPOUND = "chemical_compound"
+    MEDICATION_REMEDY = "medication_remedy"
+    
+    # Abstract Concepts and Ideas
     CONCEPT_IDEA = "concept_idea"
+    QUALITY_ATTRIBUTE = "quality_attribute"
+    MENTAL_CONSTRUCT = "mental_construct"
+    KNOWLEDGE_DOMAIN = "knowledge_domain"
+    QUANTITATIVE_CONCEPT = "quantitative_concept"
     EMOTION_FEELING = "emotion_feeling"
+    
+    # Processes and Time
     PROCESS_EVENT = "process_event"
     TIME_PERIOD = "time_period"
+    
+    # Groups and Collections
     GROUP_PEOPLE = "group_people"
-    GROUP_ANIMAL = "group_animal"
+    GROUP_ANIMAL = "animal_grouping_term"
     COLLECTION_THINGS = "collection_things"
+    
+    # Named Entities
     PERSONAL_NAME = "personal_name"
     PLACE_NAME = "place_name"
     ORGANIZATION_NAME = "organization_name"
+    
+    # Other
     OTHER = "other"
 
 class VerbSubtype(enum.Enum):
