769ff35b0de1506b8b9450471011921cd455225c
Alexander Power <powera@gmail.com>
2025-10-17 13:51:38 -0500

    clean up __init__ files.

--- src/wordfreq/translation/__init__.py
+++ src/wordfreq/translation/__init__.py
@@ -1,1 +1,6 @@
 """Machine translation and linguistic analysis using LLMs."""
+
+from wordfreq.translation.client import LinguisticClient
+from wordfreq.translation.processor import WordProcessor
+
+__all__ = ['LinguisticClient', 'WordProcessor']
