03590e2d56d824cd0aa7e214d523e01650ace77e
powera <powera@gmail.com>
2026-03-25 10:21:57 -0500

    Move rhyme key logic into top-level ipa package

--- src/ipa/__init__.py
+++ src/ipa/__init__.py
@@ -6,10 +6,24 @@ from ipa.normalization import (
     normalize_ipa,
     weighted_similarity_ratio,
 )
+from ipa.rhyme_keys import (
+    IPA_VOWELS,
+    RHYME_KEY_LANGUAGES,
+    compute_rhyme_key,
+    rhyme_key_final_sound,
+    rhyme_key_penultimate_sound,
+    rhyme_keys_available,
+)
 
 __all__ = [
     "normalize_ipa",
     "weighted_similarity_ratio",
     "are_ipa_equivalent",
     "is_ipa_levenshtein_match",
+    "RHYME_KEY_LANGUAGES",
+    "IPA_VOWELS",
+    "rhyme_keys_available",
+    "compute_rhyme_key",
+    "rhyme_key_final_sound",
+    "rhyme_key_penultimate_sound",
 ]
